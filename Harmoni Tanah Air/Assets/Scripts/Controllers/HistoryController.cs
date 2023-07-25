6a15e006075f31bb60104bf686f395e831634ea7
Andri Firman Saputra <andrifirmansaputra1@gmail.com>
2023-07-25 17:05:13 +0700

    fixed but trickey

--- Harmoni Tanah Air/Assets/Scripts/Controllers/HistoryController.cs
+++ Harmoni Tanah Air/Assets/Scripts/Controllers/HistoryController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class HistoryController : MonoBehaviour
@@ -18,37 +19,45 @@ public class HistoryController : MonoBehaviour
             Destroy(child.gameObject);
         }
 
+
         foreach (StoryScene scene in gameController.history)
         {
-            for (int i = 0; i < gameController.dialogBar.GetSentenceIndex() + 1; i++)
+            List<StoryScene.Sentence> sentencesForHistory = new List<StoryScene.Sentence>();
+
+            int maxIndex = Mathf.Min(scene.sentences.Count, gameController.dialogBar.GetSentenceIndex() + 1);
+
+            for (int i = 0; i < maxIndex; i++)
             {
-                Debug.Log("i: " + i);
-                Debug.Log("dialogsentenceindex: " + gameController.dialogBar.GetSentenceIndex() + 1);
-                StoryScene.Sentence sentence = scene.sentences[i];
+                sentencesForHistory.Add(scene.sentences[i]);
+            }
 
-                Speaker speaker = sentence.speaker;
-                string speakerName = speaker.speakerName;
-                Color speakerColor = speaker.textColor;
-
-                GameObject historyDialogBar = Instantiate(historyDialogBarPrefab, Vector3.zero, Quaternion.identity, contentHistory.transform);
-
-                var textbox = historyDialogBar.GetComponent<HistoryDialogBarController>().textBoxText;
-                textbox.text = sentence.text;
-
-                var namebox = historyDialogBar.GetComponent<HistoryDialogBarController>();
-
-                if (speakerName != "Narrator")
+            foreach (StoryScene.Sentence sentence in sentencesForHistory)
+            {
+                if (scene.sentences != sentencesForHistory)
                 {
-                    namebox.nameBoxUI.SetActive(true);
+                    Speaker speaker = sentence.speaker;
+                    string speakerName = speaker.speakerName;
+                    Color speakerColor = speaker.textColor;
 
-                    namebox.nameBoxText.text = speakerName;
-                    namebox.nameBoxText.color = speakerColor;
-                }
-                else
-                {
-                    namebox.nameBoxUI.SetActive(false);
+                    GameObject historyDialogBar = Instantiate(historyDialogBarPrefab, Vector3.zero, Quaternion.identity, contentHistory.transform);
+                    var textbox = historyDialogBar.GetComponent<HistoryDialogBarController>().textBoxText;
+                    textbox.text = sentence.text;
+
+                    var namebox = historyDialogBar.GetComponent<HistoryDialogBarController>();
+
+                    if (speakerName != "Narrator")
+                    {
+                        namebox.nameBoxUI.SetActive(true);
+                        namebox.nameBoxText.text = speakerName;
+                        namebox.nameBoxText.color = speakerColor;
+                    }
+                    else
+                    {
+                        namebox.nameBoxUI.SetActive(false);
+                    }
                 }
             }
         }
+
     }
 }
